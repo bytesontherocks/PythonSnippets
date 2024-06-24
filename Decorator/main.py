26a749d615434d6673909f1ebfa5d9adaae5463c
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-06-24 23:01:24 +0100

    playing with decorators

--- Decorator/main.py
+++ Decorator/main.py
@@ -1,12 +1,14 @@
 
-# we have a class with a transform function. In some cases we want to apply a pre-transform function to the elements of the list to be transform.
+# we have a class with a transform function. We could want to decorate the input data, or decorate the transform function depending on the input.
 # Exploring the solution using a decorator.
 
 def class_method_decorator(cls):
-    def augment(self, f_trans):       
+    
+    def augment(self, data_list, f_trans):       
         print("this is the decorator applied")
-        cls.original_transform(f_trans)
-        return
+        decorated_data = [x + "_decorated" for x in data_list]
+        #we could change the transform function depending on the input params
+        return cls.original_transform(decorated_data, f_trans)
 
     cls.original_transform = cls.transform
     cls.transform = augment
@@ -14,23 +16,38 @@ def class_method_decorator(cls):
 
 @class_method_decorator
 class A:
-
-    data = list[str]
-
-    def __init__(self, list = []):
-        self.data = list
-
     @classmethod
-    def transform(self, transform_function):
-        for el in self.data:
+    def transform(self, data_list, transform_function):
+        for el in data_list:
             transform_function(el)
 
+class B:
+
+    def __init__(self, a :int, b : int, c :int):
+        self._a = a
+        self._b = b
+        self._c = c
+    
+    @property
+    def get_a(self):
+        return self._a
+    @property
+    def get_b(self):
+        return self._b
+    
+    @property
+    def get_c(self):
+        return self._c
 
 def main():
-    n = A(["home", "llibre", "casa"])
+    a = A()
     def f_trans(x = list[str]) : print(x)
-    n.transform(f_trans)
+    a.transform(["home", "llibre", "casa"], f_trans)
     
+    b = B(2,4,6)
+    print(b._a)
+    print(b._b)
+    print(b._c)
 
 if __name__=="__main__": 
     main() 
