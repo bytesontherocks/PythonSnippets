9ebe72962c65beabaaf083bbcc1078ae36bdfd01
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-06-24 17:34:03 +0100

    trying approches

--- Decorator/main.py
+++ Decorator/main.py
@@ -1,9 +1,15 @@
 
+# we have a class with a transform function. In some cases we want to apply a pre-transform function to the elements of the list to be transform.
+# Exploring the solution using a decorator.
 
 def class_method_decorator(cls):
-    def augment(self):
+    def augment(self, f_trans):       
         print("this is the decorator applied")
-    cls.augment = augment
+        cls.original_transform(f_trans)
+        return
+
+    cls.original_transform = cls.transform
+    cls.transform = augment
     return cls
 
 @class_method_decorator
@@ -14,14 +20,18 @@ class A:
     def __init__(self, list = []):
         self.data = list
 
+    @classmethod
     def transform(self, transform_function):
         for el in self.data:
             transform_function(el)
 
+
 def main():
-    a = A(["home", "llibre", "casa"])
+    n = A(["home", "llibre", "casa"])
     def f_trans(x = list[str]) : print(x)
-    a.transform(f_trans) 
+    n.transform(f_trans)
+    
 
 if __name__=="__main__": 
     main() 
+
